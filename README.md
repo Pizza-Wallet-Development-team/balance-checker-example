ee396d2d0690aac7a54c00d8a69238b2ca9fe175
Michael <michaelksullivan50@gmail.com>
2022-10-05 11:45:13 +0200

    Start with basic web3 connect boilerplate
    React / typescript boilerplate with connect to web3 feature

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # balance-checker-example
+
 Get a list of tokens a user holds and show balances for each one.
+
+# getting started
+
+copy url and git clone /n
+
+run yarn /n
+
+yarn start /n
---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { Buffer } from 'buffer';
+import ConnectAccount from './components/Account/ConnectAccount';
+import { Layout } from 'antd';
+import './App.css';
+import 'antd/dist/antd.css';
+import ChainSelector from './components/ChainSelector';
+
+const { Header } = Layout;
+
+const styles = {
+  layout: {
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh',
+    overflow: 'auto',
+    fontFamily: 'Sora, sans-serif',
+  },
+  header: {
+    position: 'fixed',
+    zIndex: 1,
+    width: '100%',
+    backgroundColor: 'transparent',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0px 20px',
+    paddingTop: '15px',
+  },
+  headerRight: {
+    display: 'flex',
+    gap: '10px',
+    alignItems: 'center',
+    paddingRight: '10px',
+    fontSize: '15px',
+    fontWeight: '600',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    color: '#041836',
+    marginTop: '100px',
+    padding: '10px',
+    overflow: 'auto',
+  },
+  footer: {
+    position: 'fixed',
+    textAlign: 'center',
+    width: '100%',
+    bottom: '0',
+    color: 'white',
+    backgroundColor: 'transparent',
+  },
+} as const;
+
+function App() {
+  if (!window.Buffer) window.Buffer = Buffer;
+
+  return (
+    <Layout style={styles.layout}>
+      <Header style={styles.header}>
+        <div style={styles.headerRight}>
+          <ChainSelector />
+          <ConnectAccount />
+        </div>
+      </Header>
+    </Layout>
+  );
+}
+
+export default App;
